91e45511527f9d70e532fe7ab46af213bc5447e4
Eklavya-MachineWise <eklavya.g@wathare.com>
2025-09-19 23:04:27 +0530

    Feat: boilerplate added

---
+++ src/config/config.js
@@ -0,0 +1,42 @@
+const dotenv = require("dotenv");
+const Joi = require("joi");
+const fs = require("fs");
+
+const loadEnv = () => {
+  const envPaths = [
+    path.join(process.execPath, "..", ".env"), // Next to executable
+    path.join(process.cwd(), ".env"), // Current working directory
+    path.join(process.env.PORTABLE_ENV_PATH || "", ".env"), // Custom env path];
+  ];
+  for (const envPath of envPaths) {
+    if (fs.existsSync(envPath)) {
+      dotenv.config({ path: envPath });
+      return true;
+    }
+  }
+  return false;
+};
+
+loadEnv();
+
+const envVarsSchema = Joi.object()
+  .keys({
+    NODE_ENV: Joi.string()
+      .valid("production", "development", "test")
+      .required(),
+    MQTT_HOST: Joi.string().description("Mqtt broker ip").required(),
+    MQTT_PORT: Joi.string().description("Mqtt broker port").required(),
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: "key" } })
+  .validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+module.exports = {
+  mqtt: { host: envVars.MQTT_HOST, port: envVars.MQTT_PORT },
+};
